b5e3d14854e89032b93bba26eff04329b7637418
arozniewicz <aneta.rozniewicz@droidsonroids.pl>
2015-10-15 13:28:23 +0200

    Data binding added

--- app/src/main/java/pl/droidsonroids/beerapp/model/Beer.java
+++ app/src/main/java/pl/droidsonroids/beerapp/model/Beer.java
@@ -1,10 +1,18 @@
 package pl.droidsonroids.beerapp.model;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
+
 import com.google.gson.annotations.SerializedName;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class Beer {
+import pl.droidsonroids.beerapp.BR;
+
+public class Beer extends BaseObservable {
 
     private List<Product> mProducts;
 
@@ -18,83 +26,108 @@ public class Beer {
     @SerializedName("abv") private float mAlcoholByVolume;
     @SerializedName("on_sale") private boolean mOnSale;
 
+    @BindingAdapter({"bind:imageUrl"})
+    public static void loadImage(final ImageView imageView, final String imageUrl) {
+        Picasso.with(imageView.getContext()).load(imageUrl).into(imageView);
+    }
+
+    @Bindable
     public List<Product> getProducts() {
         return mProducts;
     }
 
     public void setProducts(final List<Product> products) {
         mProducts = products;
+        notifyPropertyChanged(BR.products);
     }
 
+    @Bindable
     public String getName() {
         return mName;
     }
 
     public void setName(final String name) {
         mName = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getImageUrl() {
         return mImageUrl;
     }
 
     public void setImageUrl(final String imageUrl) {
         mImageUrl = imageUrl;
+        notifyPropertyChanged(BR.imageUrl);
     }
 
+    @Bindable
     public String getCategory() {
         return mCategory;
     }
 
     public void setCategory(final String category) {
         mCategory = category;
+        notifyPropertyChanged(BR.category);
     }
 
+    @Bindable
     public String getType() {
         return mType;
     }
 
     public void setType(final String type) {
         mType = type;
+        notifyPropertyChanged(BR.type);
     }
 
+    @Bindable
     public String getBrewer() {
         return mBrewer;
     }
 
     public void setBrewer(final String brewer) {
         mBrewer = brewer;
+        notifyPropertyChanged(BR.brewer);
     }
 
+    @Bindable
     public String getCountry() {
         return mCountry;
     }
 
     public void setCountry(final String country) {
         mCountry = country;
+        notifyPropertyChanged(BR.country);
     }
 
+    @Bindable
     public long getId() {
         return mId;
     }
 
     public void setId(final long id) {
         mId = id;
+        notifyPropertyChanged(BR.id);
     }
 
+    @Bindable
     public float getAlcoholByVolume() {
         return mAlcoholByVolume;
     }
 
     public void setAlcoholByVolume(final float alcoholByVolume) {
         mAlcoholByVolume = alcoholByVolume;
+        notifyPropertyChanged(BR.alcoholByVolume);
     }
 
+    @Bindable
     public boolean isOnSale() {
         return mOnSale;
     }
 
     public void setOnSale(final boolean onSale) {
         mOnSale = onSale;
+        notifyPropertyChanged(BR.onSale);
     }
 }
--- app/src/main/java/pl/droidsonroids/beerapp/model/Product.java
+++ app/src/main/java/pl/droidsonroids/beerapp/model/Product.java
@@ -1,8 +1,16 @@
 package pl.droidsonroids.beerapp.model;
 
-import com.google.gson.annotations.SerializedName;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.BindingAdapter;
+import android.widget.ImageView;
 
-public class Product {
+import com.google.gson.annotations.SerializedName;
+import com.squareup.picasso.Picasso;
+
+import pl.droidsonroids.beerapp.BR;
+
+public class Product extends BaseObservable {
 
     @SerializedName("name") private String mName;
     @SerializedName("size") private String mSize;
@@ -17,99 +25,128 @@ public class Product {
     @SerializedName("abv") private float mAlcoholByVolume;
     @SerializedName("on_sale") private boolean mOnSale;
 
+    @BindingAdapter({"bind:imageUrl"})
+    public static void loadImage(final ImageView imageView, final String imageUrl) {
+        Picasso.with(imageView.getContext()).load(imageUrl).into(imageView);
+    }
+
+    @Bindable
     public String getName() {
         return mName;
     }
 
     public void setName(final String name) {
         mName = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getSize() {
         return mSize;
     }
 
     public void setSize(final String size) {
         mSize = size;
+        notifyPropertyChanged(BR.size);
     }
 
+    @Bindable
     public String getImageUrl() {
         return mImageUrl;
     }
 
     public void setImageUrl(final String imageUrl) {
         mImageUrl = imageUrl;
+        notifyPropertyChanged(BR.imageUrl);
     }
 
+    @Bindable
     public String getCategory() {
         return mCategory;
     }
 
     public void setCategory(final String category) {
         mCategory = category;
+        notifyPropertyChanged(BR.category);
     }
 
+    @Bindable
     public String getType() {
         return mType;
     }
 
     public void setType(final String type) {
         mType = type;
+        notifyPropertyChanged(BR.type);
     }
 
+    @Bindable
     public String getBrewer() {
         return mBrewer;
     }
 
     public void setBrewer(final String brewer) {
         mBrewer = brewer;
+        notifyPropertyChanged(BR.brewer);
     }
 
+    @Bindable
     public String getCountry() {
         return mCountry;
     }
 
     public void setCountry(final String country) {
         mCountry = country;
+        notifyPropertyChanged(BR.country);
     }
 
+    @Bindable
     public long getId() {
         return mId;
     }
 
     public void setId(final long id) {
         mId = id;
+        notifyPropertyChanged(BR.id);
     }
 
+    @Bindable
     public long getBeerId() {
         return mBeerId;
     }
 
     public void setBeerId(final long beerId) {
         mBeerId = beerId;
+        notifyPropertyChanged(BR.beerId);
     }
 
+    @Bindable
     public float getPrice() {
         return mPrice;
     }
 
     public void setPrice(final float price) {
         mPrice = price;
+        notifyPropertyChanged(BR.price);
     }
 
+    @Bindable
     public float getAlcoholByVolume() {
         return mAlcoholByVolume;
     }
 
     public void setAlcoholByVolume(final float alcoholByVolume) {
         mAlcoholByVolume = alcoholByVolume;
+        notifyPropertyChanged(BR.alcoholByVolume);
     }
 
+    @Bindable
     public boolean isOnSale() {
         return mOnSale;
     }
 
     public void setOnSale(final boolean onSale) {
         mOnSale = onSale;
+        notifyPropertyChanged(BR.onSale);
     }
 }
